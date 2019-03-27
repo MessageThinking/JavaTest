f27fca6e444ece6dae16ea6942cd152ba1c3bcd2
MessageThinking <wansandawang@gmail.com>
2019-03-27 22:13:33 +0800

    Revert "revert05"
    This reverts commit 98a3cad

--- src/main/java/com/o2o/dao/ShopDao.java
+++ src/main/java/com/o2o/dao/ShopDao.java
@@ -5,17 +5,17 @@ import java.util.List;
 import com.o2o.entity.Shop;
 
 /** 
-* @author 作者：wanwei6667777
+* @author 作者：01wanwei
 * @version 创建时间：2017年11月3日 上午9:28:01 
 * 类说明 
 */
 public interface ShopDao {
 /**
- * 增添店铺
+ *wanwei增添店铺
  * @param shop
  * @return
  */
-int insertShop(Shopsssss shop);
+int insertShop(Shop shop);
 /**
  * 更新店铺
  */
