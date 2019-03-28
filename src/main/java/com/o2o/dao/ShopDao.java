2b37cccc948b03d86aceef1e4ccf967cad034bfe
MessageThinking <wansandawang@gmail.com>
2019-03-28 20:59:01 +0800

    Re22222

--- src/main/java/com/o2o/dao/ShopDao.java
+++ src/main/java/com/o2o/dao/ShopDao.java
@@ -4,13 +4,6 @@ import java.util.List;
 
 import com.o2o.entity.Shop;
 
-/** 
-* @author 作者：01wanwei111
-* @version 创建时间：2017年11月3日 上午9:28:01 
-* 类说明 
-*/
-public interface ShopDao {
-/**
  *wanwei增添店铺
  * @param shop
  * @return
